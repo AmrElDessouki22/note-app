b63eb5a401f69e5e6b8853ddf74eb08ab11ac341
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 11:44:06 +0300

    Update src/types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,4 @@
 export interface Note {
   id: string;
-  title: string;
   content: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
-
-export interface User {
-  id: string;
-  name: string;
-  email: string;
 }
