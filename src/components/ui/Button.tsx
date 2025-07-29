096fcb5eaeaee2da6e7cc731897a6228e641f23b
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 11:53:40 +0300

    Update src/components/ui/Button.tsx

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,36 +1,15 @@
 "use client";
 
-import { useState } from 'react';
-
 interface ButtonProps {
   onClick: () => void;
   children: React.ReactNode;
 }
 
 const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
-  const [note, setNote] = useState('');
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNote(event.target.value);
-  };
-
-  const handleButtonClick = () => {
-    onClick(note);
-    setNote('');
-  };
-
   return (
-    <div>
-      <input
-        type="text"
-        value={note}
-        onChange={handleInputChange}
-        placeholder="Enter note"
-      />
-      <button onClick={handleButtonClick} className="btn">
-        {children}
-      </button>
-    </div>
+    <button onClick={onClick} className="btn">
+      {children}
+    </button>
   );
 };
 
