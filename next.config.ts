6b360243a8db5919070e1f6568e6cc3e6e53ecda
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-30 14:34:30 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
 };
 
 export default nextConfig;
