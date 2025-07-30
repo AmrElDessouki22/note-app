56087820714855763b4b3f843da2da77da87da88
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-30 14:34:39 +0300

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,18 @@
-import NewNoteForm from '../components/features/NewNoteForm';
-import NoteList from '../components/features/NoteList';
-import { NotesProvider } from '../contexts/NotesContext';
+import React from 'react';
+import Header from '@/components/ui/Header';
+import Footer from '@/components/ui/Footer';
+import Notes from '@/components/features/Notes';
 
-const Page = () => {
+const HomePage: React.FC = () => {
   return (
-    <NotesProvider>
-      <div className="app-container">
-        <NewNoteForm />
-        <NoteList />
-      </div>
-    </NotesProvider>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow">
+        <Notes />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
-export default Page;
+export default HomePage;
